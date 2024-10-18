af4578c811719feee9c9a22208e480831955468f
Fabian Frank Werner <fabian@fabianfrankwerner.com>
2024-10-18 15:22:58 +0200

    Fetch Data

--- src/components/QuizPage.jsx
+++ src/components/QuizPage.jsx
@@ -1,3 +1,7 @@
+fetch("https://opentdb.com/api.php?amount=5&type=multiple")
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
 export default function QuizPage() {
   return (
     <div className="quiz">
