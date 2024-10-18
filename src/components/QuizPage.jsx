190ddb42b421ece13355a52523e89b251ba11527
Fabian Frank Werner <fabian@fabianfrankwerner.com>
2024-10-18 15:41:49 +0200

    Shuffle right answer between the wrong answers

--- src/components/QuizPage.jsx
+++ src/components/QuizPage.jsx
@@ -1,15 +1,35 @@
-/*
-
-fetch("https://opentdb.com/api.php?amount=5&type=multiple")
-  .then((response) => response.json())
-  .then((data) => console.log(data));
-
-*/
 import he from "he";
+//he.decode(html)
 
-console.log(he.decode("foo &copy; bar &ne; baz &#x1D306; qux"));
+// questionsWithShuffledAnswers
 
 export default function QuizPage() {
+  fetch("https://opentdb.com/api.php?amount=5&type=multiple")
+    .then((response) => response.json())
+    .then((data) => {
+      const questionsWithShuffledAnswers = data.results.map((question) => {
+        return {
+          ...question,
+          answers: shuffleArray([
+            ...question.incorrect_answers,
+            question.correct_answer,
+          ]),
+        };
+      });
+
+      console.log(questionsWithShuffledAnswers);
+      return questionsWithShuffledAnswers;
+    });
+
+  function shuffleArray(array) {
+    const shuffled = [...array];
+    for (let i = shuffled.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+    return shuffled;
+  }
+
   return (
     <div className="quiz">
       <p>I am the quiz page</p>
