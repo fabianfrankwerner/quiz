9546dabccaeaabaaddf349c637e019629a472617
Fabian Frank Werner <fabian@fabianfrankwerner.com>
2024-10-18 15:33:16 +0200

    Import he

--- src/components/QuizPage.jsx
+++ src/components/QuizPage.jsx
@@ -1,6 +1,13 @@
+/*
+
 fetch("https://opentdb.com/api.php?amount=5&type=multiple")
   .then((response) => response.json())
   .then((data) => console.log(data));
+
+*/
+import he from "he";
+
+console.log(he.decode("foo &copy; bar &ne; baz &#x1D306; qux"));
 
 export default function QuizPage() {
   return (
