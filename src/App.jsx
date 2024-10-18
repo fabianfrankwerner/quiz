f0715ea55e7b9279d315ebbdc02cacd9af5d6fab
Fabian Frank Werner <fabian@fabianfrankwerner.com>
2024-10-18 14:56:21 +0200

    Add LandingPage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
+import LandingPage from "./components/LandingPage";
+
 export default function App() {
-  return (
-    <>
-      <h1>Quiz</h1>
-      <p>Test your knowledge.</p>
-      <button>Start quiz</button>
-    </>
-  );
+  return <LandingPage />;
 }
---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,9 @@
+export default function LandingPage() {
+  return (
+    <>
+      <h1>Quiz</h1>
+      <p>Test your knowledge.</p>
+      <button>Start quiz</button>
+    </>
+  );
+}
