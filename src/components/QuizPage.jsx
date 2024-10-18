5f8c08241a810029642d0257f28398de0651efe2
Fabian Frank Werner <fabian@fabianfrankwerner.com>
2024-10-18 16:31:06 +0200

    Add Answers

--- src/components/QuizPage.jsx
+++ src/components/QuizPage.jsx
@@ -1,28 +1,31 @@
 import he from "he";
-// questionsWithShuffledAnswers
+import { useState, useEffect } from "react";
 
 export default function QuizPage() {
-  fetch("https://opentdb.com/api.php?amount=5&type=multiple")
-    .then((response) => response.json())
-    .then((data) => {
-      const questionsWithShuffledAnswers = data.results.map((question) => {
-        return {
-          ...question,
-          question: he.decode(question.question),
-          correct_answer: he.decode(question.correct_answer),
-          incorrect_answers: question.incorrect_answers.map((answer) =>
-            he.decode(answer)
-          ),
-          answers: shuffleArray([
-            ...question.incorrect_answers.map((answer) => he.decode(answer)),
-            he.decode(question.correct_answer),
-          ]),
-        };
-      });
+  const [questions, setQuestions] = useState([]);
 
-      console.log(questionsWithShuffledAnswers);
-      return questionsWithShuffledAnswers;
-    });
+  useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=5&type=multiple")
+      .then((response) => response.json())
+      .then((data) => {
+        const questionsWithShuffledAnswers = data.results.map((question) => {
+          return {
+            ...question,
+            question: he.decode(question.question),
+            correct_answer: he.decode(question.correct_answer),
+            incorrect_answers: question.incorrect_answers.map((answer) =>
+              he.decode(answer)
+            ),
+            answers: shuffleArray([
+              ...question.incorrect_answers.map((answer) => he.decode(answer)),
+              he.decode(question.correct_answer),
+            ]),
+          };
+        });
+
+        setQuestions(questionsWithShuffledAnswers);
+      });
+  }, []);
 
   function shuffleArray(array) {
     const shuffled = [...array];
@@ -35,7 +38,26 @@ export default function QuizPage() {
 
   return (
     <div className="quiz">
-      <p>I am the quiz page</p>
+      {questions.map((question, questionIndex) => (
+        <div key={questionIndex} className="question-container">
+          <h2>{question.question}</h2>
+          <div className="answers">
+            {question.answers.map((answer, answerIndex) => (
+              <div key={answerIndex} className="answer">
+                <input
+                  type="radio"
+                  id={`q${questionIndex}-a${answerIndex}`}
+                  name={`question-${questionIndex}`}
+                  value={answer}
+                />
+                <label htmlFor={`q${questionIndex}-a${answerIndex}`}>
+                  {answer}
+                </label>
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
